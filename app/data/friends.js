ccbce28bac92fff546d51ba8015fdabb05e464eb
malcolmberry96 <malcolm.berry65@gmail.com>
2018-11-11 15:12:12 -0600

    initial commit

---
+++ app/data/friends.js
@@ -0,0 +1,21 @@
+//array that holds all the 'friends' that have taken the survey
+//initialize starting with 'dummy' data
+var friendList = [
+    {
+      name: 'Malcolm Berry',
+      profilePic: 'https://avatars2.githubusercontent.com/u/39191969?s=400&u=e1dbb0981aeaeb1974cf376fa0ce769a809cf337&v=4',
+      scores: [
+        "4",
+        "5",
+        "5",
+        "2",
+        "4",
+        "5",
+        "4",
+        "4",
+        "3",
+        "4"
+      ]
+    }
+  ]
+  
